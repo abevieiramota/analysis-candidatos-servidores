6d6a1bbea1b7c288d80e30b0316b09421bc422b3
abevieiramota <abevieiramota@gmail.com>
2016-07-05 20:22:28 -0300

    adicionado report de candidatos CE servidores federal

--- sql/candidato_servidor_total_despesa_leq_2000.sql
+++ sql/candidato_servidor_total_despesa_leq_2000.sql
@@ -1,29 +1,13 @@
-﻿select c.nome_candidato, total.total_despesa, d.*
-from candidato c
-inner join despesa d on (c.sequencial_candidato = d.sequencial_candidato)
+﻿select cc.nome_candidato, cc.despesa_max_campanha, total.total_despesa, s.org_lotacao, s.descricao_cargo, s.funcao, s.regime_juridico, cc.descricao_cargo, cc.descricao_ocupacao, cc.desc_sit_tot_turno
+from candidato cc
 inner join 
 (
-	select d.sequencial_candidato, sum(valor_despesa) as total_despesa
-	from despesa d
-	inner join (
-		-- servidores candidatos
-		select c.sequencial_candidato
-		from candidato c
-		inner join servidor s on (c.cpf_candidato_parcial = s.cpf_parcial and c.nome_candidato = s.nome)
-	) as sc on (d.sequencial_candidato = sc.sequencial_candidato)
-	group by d.sequencial_candidato
-	having sum(valor_despesa) <= 2000
-	order by sum(valor_despesa) asc
-) as total on (c.sequencial_candidato = total.sequencial_candidato)
-order by total.total_despesa asc;
-
-select descricao_cargo, desc_sit_tot_turno, count(*)
-from candidato c
-where not exists
-(
-	select 1
-	from despesa d
-	where d.sequencial_candidato = c.sequencial_candidato
-)
-and num_turno = 1
-group by desc_sit_tot_turno, descricao_cargo;
+	select c.sequencial_candidato, sum(valor_despesa) as total_despesa
+	from candidato c
+	inner join despesa d using (sequencial_candidato)
+	where num_turno = 1
+	group by c.sequencial_candidato
+) as total using (sequencial_candidato)
+inner join servidor s on (s.nome = cc.nome_candidato and s.cpf_parcial = cc.cpf_candidato_parcial)
+where num_turno = 1
+order by total.total_despesa desc;
