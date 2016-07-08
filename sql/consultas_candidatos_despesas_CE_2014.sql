ba4a106324eb7775b5fce635e411e981004fc4f5
abevieiramota <abevieiramota@gmail.com>
2016-07-08 18:04:02 -0300

    adicionado reports de servidores candidatos 2012 CE

--- sql/consultas_candidatos_despesas_CE_2014.sql
+++ sql/consultas_candidatos_despesas_CE_2014.sql
@@ -1,4 +1,12 @@
-﻿select cc.nome_candidato, cc.despesa_max_campanha, total.total_despesa, s.org_lotacao, s.descricao_cargo, s.funcao, s.regime_juridico, cc.descricao_cargo, cc.descricao_ocupacao, cc.desc_sit_tot_turno
+﻿select 
+cc.nome_candidato as nome, 
+total.total_despesa, 
+cc.descricao_cargo as candidato_cargo, 
+s.descricao_cargo as servidor_cargo, 
+s.org_lotacao as servidor_lotacao, 
+s.funcao as servidor_funcao, 
+cc.descricao_ocupacao as candidato_ocupacao, 
+cc.desc_sit_tot_turno as candidato_situacao_turno_1
 from candidato cc
 inner join 
 (
@@ -10,4 +18,15 @@ inner join
 ) as total using (sequencial_candidato)
 inner join servidor s on (s.nome = cc.nome_candidato and s.cpf_parcial = cc.cpf_candidato_parcial)
 where num_turno = 1
-order by total.total_despesa desc;
+order by total.total_despesa asc;
+
+select *
+from despesa
+where sequencial_candidato in
+(
+	select 
+	cc.sequencial_candidato
+	from candidato cc
+	inner join servidor s on (s.nome = cc.nome_candidato and s.cpf_parcial = cc.cpf_candidato_parcial)
+)
+order by nome_candidato asc
